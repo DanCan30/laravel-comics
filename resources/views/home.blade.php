e9e3f6f31e24efa1f523a5c79108b58bb0286840
DanCan30 <danielecan3008@gmail.com>
2022-09-07 17:10:28 +0200

    Main content added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,20 @@
 
 @section("main-content")
 
+    <div>
+        @foreach ($comics as $comic)
+            <div class="card">
+
+            <div class="comic-img-container">
+                <img src="{{ $comic->image }}" alt="{{ $comic->title }}">
+            </div>
+            <h4>{{ $comic->title }}</h4>
+
+            </div>       
+        @endforeach
+    </div>
+    <div>
+        <a href="#">load more</a>
+    </div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comicsList = config("comics");
+    return view('home', ["comics" => $comicsList]);
 });
